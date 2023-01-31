6bc906af21653564fa97dc7c39389a3162a6a13a
Nick Scofield <c.nick.scofield@gmail.com>
2023-01-31 09:36:49 -0700

    commit tree 5b864baaa014af0f3d6af822f7b2f4fccd854cf4

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
-class Dog
+require "pry"
 
+class Dog
+    def name=(name)
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
+
+spot = Dog.new
+spot.name= "Spot"
+
+# binding.pry
